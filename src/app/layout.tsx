d76b8baa8dcad6974e20cc1efdff1df74c3ad593
Animesh-roy100 <royanimesh7478@gmail.com>
2024-03-14 01:14:38 +0530

    Navbar and Icons component added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
-  title: "techTalks",
+  title: "TechTalk",
   description: "A Reddit clone for developers, built with Next.js",
 };
 
@@ -23,7 +24,10 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        {children}
+        <Navbar />
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
       </body>
     </html>
   );
